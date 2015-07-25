fd9366259ce38def0577c22058aff70040a1649e
Raymond Mariano <rcm@hawaii.edu>
2015-07-25 12:37:53 -1000

    added speak variable to replace chirp

--- chirp.js
+++ chirp.js
@@ -1,17 +1,25 @@
 module.exports = chirp;
 
-function chirp(num) {
+function chirp(num, word) {
+  var speak = "chirp";
+
   if(typeof num !== "number"){
     return 'cock-a-doodle-doo!';
   }
+
+  if(word !== undefined &&
+   typeof word === 'string') {
+    speak = word;
+  }
+
   if(num <= 0){
     return "";
   }
   if(num === 1) {
-    return "chirp" + chirp(num-1);
+    return speak + chirp(num-1, word);
   }
   if(num > 1) {
-    return "chirp " + chirp(num-1);
+    return speak + " " + chirp(num-1, word);
   }
 
 }
--- test/spec.js
+++ test/spec.js
@@ -1,7 +1,19 @@
 var chai = require('chai');
 var expect = chai.expect;
-var should = chai.should;
+var should = chai.should();
 var chirp = require('../chirp.js');
+var sandbox;
+
+// beforeEach(function() {
+//   // create a sandbox
+//   sandbox = sinon.sandbox.create();
+//   sinon.spy(window.console, "log");
+// });
+
+// afterEach(function() {
+//   sandbox.restore();
+//   console.log.restore();
+// });
 
 describe('chirp', function() {
   it('should be a function', function() {
@@ -16,8 +28,19 @@ describe('chirp', function() {
   it('should return "cock-a-doodle-doo!" if num is not a number', function() {
     expect(chirp('b')).to.equal('cock-a-doodle-doo!');
   });
+  it('should return an empty string if num is 0 or less', function() {
+    expect(chirp(0)).to.equal("");
+    expect(chirp(-5)).to.equal("");
+  });
 
   it('should return correct answer', function() {
+    expect(chirp(2)).to.equal('chirp chirp');
     expect(chirp(3)).to.equal('chirp chirp chirp');
+    expect(chirp(4)).to.equal('chirp chirp chirp chirp');
+  });
+
+  it('should replace "chirp" with word parameter', function() {
+    expect(chirp(2, "stahp")).to.equal('stahp stahp');
+    expect(chirp(3, "YES!")).to.equal('YES! YES! YES!');
   });
 });
